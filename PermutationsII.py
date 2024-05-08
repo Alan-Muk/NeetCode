ffc602312a61969ba1c00c5df829d6d7b160a95a
Alan-Muk <alanmukab@gmail.com>
2024-05-08 08:34:46 +0300

    Backtracking

---
+++ PermutationsII.py
@@ -0,0 +1,25 @@
+class Solution:
+	def permuteUnique(self, nums:list[int]) -> list[list[int]]:
+		res = []
+		perm = []
+		count = { n:0 for n in mums }
+		for n in nums:
+			count[n] += 1
+
+		def dfs():
+			if len(perm) == len(nums):
+				res.append(perm.copy())
+				return
+
+			for n in count:
+				if count[n] > 0:
+					perm.append(n)
+					count[n] -= 1
+
+					dfs()
+
+					count[n] += 1
+					perm.pop()
+
+		dfs()
+		return res
